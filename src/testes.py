14ab063cc1c2176b1a41c4e2abdfd0e10e2eb5ba
gemontgomeryf <36682762+gemontgomeryf@users.noreply.github.com>
2018-03-13 21:53:59 -0300

    quarta versao de atualização identação

--- src/testes.py
+++ src/testes.py
@@ -2,15 +2,13 @@ import jogovelha
 import sys
 
 erroInicializar = False
-
 jogo = jogovelha.inicializar()
 
-if len(jogo) != 3:
+if len (jogo) !=3:
   erroInicializar = True
-
 else:
   for linha in jogo:
-    if len(linha) != 3:
+    if len (linha) != 3:
       erroInicializar = True
     else:
       for elemento in linha:
